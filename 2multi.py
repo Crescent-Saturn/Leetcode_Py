4379758949b99f423d29ad25fb852f23d532d96c
Lei <Crescent-Saturn@users.noreply.github.com>
2018-01-30 15:56:04 -0500

    From 2sum to 2multi

---
+++ 2multi.py
@@ -0,0 +1,13 @@
+def twoMult(array, product):
+    dict = {}
+    for i in range(len(array)):
+        x = array[i]
+        if product / x in dict:
+            return (dict[product // x] + 1, i + 1)
+        dict[x] = i
+
+
+A = [2, 4, 1, 6, 5, 40, -1]
+M = 30
+
+print(twoMult(A, M))
