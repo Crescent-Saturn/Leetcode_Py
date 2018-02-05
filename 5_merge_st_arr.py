858a9aed7b3a026a7cfafc3db67ad8615b3d30f4
Lei <Crescent-Saturn@users.noreply.github.com>
2018-02-05 17:37:24 -0500

    Update Q.5 Merge sorted array

--- 5_merge_st_arr.py
+++ 5_merge_st_arr.py
@@ -17,3 +17,24 @@ def merge_arr(A, m, B, n):
             A[i] = B[k]
             k -= 1
         i -= 1
+
+
+def me_sorted(A, B):
+    out = []
+    while A and B:
+        if A[0] <= B[0]:
+            out.append(A.pop(0))
+        else:
+            out.append(B.pop(0))
+    while A:
+        out.append(A.pop(0))
+    while B:
+        out.append(B.pop(0))
+
+    return out
+
+
+A = [1, 3, 5, 7, 9]
+B = [2, 4, 6, 8, 10]
+
+print(me_sorted(A, B))
